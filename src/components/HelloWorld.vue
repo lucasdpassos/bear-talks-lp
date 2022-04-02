9574f54e77e408b1f407b6fe4450566e7800ea3a
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-04-02 17:16:13 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="container max-h-screen">
+<div class="flex w-screen h-[600px] bg-primary flex-row">  
+  <div class="basis-1/3 flex flex-row justify-center items-center">
+  <img src="../assets/logo.png" />
+  </div>
+  <div class="bg-primary w-[980px] flex flex-row justify-end items-end">
+    <div class="absolute flex flex-col mb-[200px] mr-[700px]">
+    <div class="flex p-4 hover:p-2">
+      <button class="">
+      <img src="../assets/ourtechbtn.png" class="max-w-[300px]" />
+      </button>
+    </div>
+     <div class="flex p-4 hover:p-2">
+      <button class="">
+      <img src="../assets/whobtn.png" class="max-w-[300px]" />
+      </button>
+    </div>
+    <div class="flex p-4 hover:p-2">
+      <button class="">
+      <img src="../assets/sucesstbn.png" class="max-w-[300px]" />
+      </button>
+    </div>
+    </div>
+    <img src="../assets/flashlight.png" class="absolute ml-[-290px] mt-[80px]" />
+    <img src="../assets/herosection1.png" class="max-w-[400px]" />  
+  </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    colors: {     
+      primary: "#27272a",
+      secondary: "#e7e5e4"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
