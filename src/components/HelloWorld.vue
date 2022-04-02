109b8c6e17ae22124c5a5103c4d16db3c0bdb780
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-04-02 18:27:33 -0300

    hero section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,21 @@
 <template>
 <div class="container max-h-screen">
-<div class="flex w-screen h-[600px] bg-primary flex-row">  
+<div class="flex w-screen h-[500px] bg-primary flex-row items-center justify-evenly">  
   <div class="basis-1/3 flex flex-row justify-center items-center">
   <img src="../assets/logo.png" />
   </div>
-  <div class="bg-primary w-[980px] flex flex-row justify-end items-end">
-    <div class="absolute flex flex-col mb-[200px] mr-[700px]">
-    <div class="flex p-4 hover:p-2">
-      <button class="">
-      <img src="../assets/ourtechbtn.png" class="max-w-[300px]" />
-      </button>
-    </div>
-     <div class="flex p-4 hover:p-2">
-      <button class="">
-      <img src="../assets/whobtn.png" class="max-w-[300px]" />
-      </button>
-    </div>
-    <div class="flex p-4 hover:p-2">
-      <button class="">
-      <img src="../assets/sucesstbn.png" class="max-w-[300px]" />
-      </button>
-    </div>
-    </div>
-    <img src="../assets/flashlight.png" class="absolute ml-[-290px] mt-[80px]" />
-    <img src="../assets/herosection1.png" class="max-w-[400px]" />  
+  <div class="flex flex-col gap-8 align-middle justify-evenly">
+  <HeroSectionButton heroBtnMessage="Cases de Sucesso" />  
+  <HeroSectionButton heroBtnMessage="Nossa Tecnologia" />  
+  <HeroSectionButton heroBtnMessage="Equipe" />  
   </div>
   </div>
 </div>
 </template>
 
-<script>
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
-
+import HeroSectionButton from './heroSectionButton.vue';
 </script>
---
+++ src/components/heroSectionButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="bg-secondary px-3 py-3 rounded-md hover:py-5">
+    <span>{{ heroBtnMessage }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    heroBtnMessage: String
+  }
+}
+</script>
