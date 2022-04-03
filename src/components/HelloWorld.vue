1e0ff3f086b7082a1aaa5da0326907c1f2700dab
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-04-03 17:46:59 -0300

    testing vite json import

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,18 @@
 <div class="container max-h-screen">
 <div class="flex w-screen h-[500px] bg-primary flex-row items-center justify-evenly">  
   <div class="basis-1/3 flex flex-row justify-center items-center">
-  <img src="../assets/logo.png" />
+  <img src="../assets/lucaskatana.png" />
   </div>
   <div class="flex flex-col gap-8 align-middle justify-evenly">
-  <HeroSectionButton heroBtnMessage="Cases de Sucesso" />  
-  <HeroSectionButton heroBtnMessage="Nossa Tecnologia" />  
-  <HeroSectionButton heroBtnMessage="Equipe" />  
+  <h1 class="text-secondary text-3xl">{{ personalData.name }}</h1>
   </div>
   </div>
 </div>
 </template>
 
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 
 import HeroSectionButton from './heroSectionButton.vue';
+import { personalData } from './example.json'
+
 </script>
