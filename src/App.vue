bbad75ed2bb730d3628ca8e11edc117364f67c3b
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-04-04 23:17:28 -0300

    start to training tables

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import WhoAmI from './components/WhoAmI.vue';
+import Learned from './components/Learned.vue';
 </script>
 
 <template>  
   <HelloWorld />
   <WhoAmI />
+  <Learned />
 </template>
 
---
+++ src/components/Learned.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="container max-h-screen scroll-smooth">
+<div class="bg-secondary flex justify-center align-middle items-center flex-col w-screen h-[500px]">
+
+<table class="table-auto">
+  <thead>
+    <tr>
+      <th>Conteúdo</th>
+      <th>Aprendido</th>      
+    </tr>
+  </thead>
+ <tbody class="bg-primary text-secondary ">
+    <tr>
+      <td v-for="item in items" class="bg-tomato">{{item}}</td>
+      <td class="bg-tomato">Malcolm Lockyer</td>
+        
+    </tr>
+    <tr>
+      <td>Witchy Woman</td>
+      <td>The Eagles</td>
+    
+    </tr>
+    <tr>
+      <td>Shining Star</td>
+      <td>Earth, Wind, and Fire</td>
+
+    </tr>
+  </tbody>
+</table>
+</div>
+    
+</div>
+</template>
+
+
+<script>
+import learned from '../components/studies.json'
+
+export default {
+    data() {
+        return {
+             items:learned
+        }
+    }
+}
+</script>
